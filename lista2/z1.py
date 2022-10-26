7391c6f317b6e6070b607cb4d6f768ceac92cb52
kapiura <swinxten@gmail.com>
2022-10-26 13:13:20 +0200

    wefew

--- lista2/z1.py
+++ lista2/z1.py
@@ -1,2 +1,3 @@
-for x in range(20):
-    print(2)
+import math
+for i in range(20):
+    print (1.2*math.e+3+34.5, end="; ")
---
+++ lista2/z3.py
@@ -0,0 +1,4 @@
+napis=input()
+napis = [literki for literki in napis]
+end = len(napis)-1
+print (napis[0],napis[1],napis[end-1],napis[end], sep="")
