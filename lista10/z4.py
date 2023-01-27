c9a0dce7be28a7a79fd218837e68da5f7c4ba83c
kapiura <swinxten@gmail.com>
2023-01-27 15:50:18 +0100

    4. Waluty

--- lista10/z4.py
+++ lista10/z4.py
@@ -1,26 +1,69 @@
-import numpy as np
-import sys
+from bs4 import BeautifulSoup as bs
+import os
+
+class Przelicznik_walut:
+    def __init__(self,path):
+        self.path = path
+        with open(path, 'rb') as f_in:
+            self.xml_soup = bs(f_in.read(), 'xml')
+        self.kursy = {}
+        for i in self.xml_soup.find_all('pozycja'):
+            self.kursy[i.kod_waluty.string] = float(i.kurs_sredni.string.replace(',','.'))
+    def wyswietl_kursy(self):
+        for i in self.kursy:
+            print(i, self.kursy[i])
+    def przelicz_pln_na_inna(self,kwota,waluta):
+        es = str(round(kwota/self.kursy[waluta],2))
+        es = es+' '+waluta
+        return es
+    def przelicz_inna_na_pln(self,kwota,waluta):
+        es = str(round(kwota*self.kursy[waluta],2))
+        es = es+' '+waluta
+        return es
+    def przelicz_inna_na_inna(self,kwota,waluta,waluta2):
+        es = str(round(kwota*self.kursy[waluta]/self.kursy[waluta2],2))
+        es = es+' '+waluta2
+        return es
+
+        
 
 
+def wybor():
+    print('\n1. Wyswietl kurs walut\n2. Konwersja z pln na wybrana walute i na odwrot\n3. Konwersja wybranej waluty na inna wybrana walute\n')
 
-if len(sys.argv) == 1:
-    dane = [int(line) for line in sys.stdin]
-else:
-    dane = sys.argv[1]
-    prze = np.array(dane)
-    dane = np.char.split(prze,sep=',')
-    dane = dane.tolist()
-    dane = [eval(i) for i in dane]
-
-
-
-
-def obl(dane):
-    s = np.average(dane)
-    w = np.var(dane)
-    o = np.std(dane)
-    print("Srednia : ", s)
-    print("Wariancja : ", w)
-    print("Odchylenie standardowe : ", o)
-
-obl(dane)
+if __name__ == '__main__':
+    os.system('clear')
+    p = Przelicznik_walut('/home/kapiura/JSP2022/lista10/kursy.xml')
+    while(True):
+        wybor()
+        wyr = int(input("Wybierz instrukcje: "))
+        match wyr:
+            case 1 :
+                os.system('clear')
+                p.wyswietl_kursy()
+            case 2:
+                wyr2 = int(input('1. Pln na inna\n2. Inna na pln: '))
+                match wyr2:
+                    case 1:
+                        os.system('clear')
+                        kwota = int(input('Podaj kwote: '))
+                        waluta = str(input('Podaj walute: '))
+                        print(p.przelicz_pln_na_inna(kwota,waluta))
+                    case 2:
+                        os.system('clear')
+                        kwota = int(input('Podaj kwote: '))
+                        waluta = str(input('Podaj walute: '))
+                        print(p.przelicz_inna_na_pln(kwota,waluta))
+                    case _:
+                        os.system('clear')
+                        print('Nie ma takiej instrukcji')
+                        break
+            case 3:
+                os.system('clear')
+                kwota = int(input('Podaj kwote: '))
+                waluta = str(input('Podaj walute: '))
+                waluta2 = str(input('Podaj walute: '))
+                print(p.przelicz_inna_na_inna(kwota,waluta,waluta2))
+            case _:
+                print('Nie ma takiej instrukcji')
+                break
