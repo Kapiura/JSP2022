9434fdd2ae736bf79cd97a6ed687afecb29ee4e0
kapiura <swinxten@gmail.com>
2023-01-27 15:53:53 +0100

    lista10

--- lista10/z2.py
+++ lista10/z2.py
@@ -1,45 +1,15 @@
-
+import itertools as it
 test = [1,2,3]
 
 
 class Klasa():
-    def __init__(self,l):
+    def __init__(self,l:list):
         self.lista = l
     def podlisty(self):
-        podlisty = []
-        #appending sublists
-        for i in range(0,len(self.lista)):
-            for j in range(len(self.lista)):
-                    podlisty.append(self.lista[i:i+j])
-
-        # #deleting duplicates
-        # for b in podlisty:
-        #     i = len(podlisty)-1
-        #     czy = 0
-        #     temp = b
-        #     while i >= 0:
-        #         if czy != 0 and podlisty[i]==temp:
-        #             podlisty.remove(podlisty[i])
-        #         if podlisty[i] == temp:
-        #             czy = 1
-        #         i-=1
-
-        
-        return podlisty
+        self.lists = []
+        for i in range(len(self.lista)+1):
+            self.lists += [list(j) for j in it.combinations(self.lista, i)]
+        return self.lists
 
 ziut = Klasa(test)
 print(ziut.podlisty())
-
-len=3
-i = 0
-j = 0
-[]
-i=0
-j=1
-[1]
-i=0
-j=2
-[1,2]
-i=0
-j=3
-[1,2,3]
--- lista10/z3.py
+++ lista10/z3.py
@@ -1,9 +1,18 @@
-#z2 ziuta essa lul
-#dziedziczenie 
+import itertools as it
+test = [5,7,2,1,-3,-5,-1,-2,0,5]
 
-class Y(X):
-    def __init__(self,[:lista]):
-        X.__init__(self,l)
-        for lista in self.podlisty:
-            if len(podlista)==3 and sum(lista)==0:
-                self.podlisty0.append()
+
+class Klasa():
+    def __init__(self,l:list):
+        self.lista = l
+    def podlisty(self):
+        self.lists = []
+        self.temp = []
+        self.temp += [list(j) for j in it.combinations(self.lista, 3)]
+        for k in self.temp:
+            if sum(k) == 0:
+                self.lists.append(k)
+        return self.lists
+
+ziut = Klasa(test)
+print(ziut.podlisty())
